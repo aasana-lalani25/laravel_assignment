8940af34a5f425982a930ed586879a14b0a53459
aasana-lalani25 <aasanalalani25@gmail.com>
2025-01-22 08:32:09 +0530

    Add files via upload

---
+++ views/blog.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <h1>This is blog page</h1>
+    
+
+@endsection
---
+++ views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+    <h1> welcome to our website</h1>
+    <p> this is home page of our basic laravel website </p>
+@endsection
