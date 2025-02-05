65cff6bd255228e7f451e7460c064438aa4e7f7a
aasana-lalani25 <aasanalalani25@gmail.com>
2025-02-05 17:23:33 +0530

    Delete views directory

--- views/blog.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('content')
-    <h1>This is blog page</h1>
-    
-
-@endsection
--- views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-
-@section('content')
-    <h1> welcome to our website</h1>
-    <p> this is home page of our basic laravel website </p>
-@endsection
