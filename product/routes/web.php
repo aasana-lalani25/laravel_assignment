a22f57300137247e6980ce884985e50b3b6c28cf
aasana-lalani25 <aasanalalani25@gmail.com>
2025-02-06 11:54:29 +0530

    Delete product directory

--- product/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\productdemo;
-
-
-Route::post('/submitform',action:[productdemo::class ,'submitform']);
-Route::get('/',action:[productdemo::class,'index']);
- 
